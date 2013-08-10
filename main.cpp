5e49084bee8994c079df87773078e226748e5bfb
AlexeyMilyakov <AlexeyMilyakov@0603119b-e3c3-4a3d-9267-139a17c484d8>
2013-08-10 14:01:03 +0000

    Chroma planes are cleared before encoding, Bit order for MD data is changed. FPGA is resetted when streaming is started. New boundary values are applied for MD settings.
    git-svn-id: https://borisvanin.dyndns.org/minerva-svn/arm-enc-app@285 0603119b-e3c3-4a3d-9267-139a17c484d8

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ public:
 	virtual void Stop() {g_stop = true;}
 
 	virtual std::string GetEncCfg() {
-		std::ifstream cfg(std::string("encoder_console.cfg"));
+		std::ifstream cfg(std::string("encoder.cfg"));
 		if (!cfg)
 			return std::string();
 		std::string str(std::istreambuf_iterator<char>(cfg), (std::istreambuf_iterator<char>()));
@@ -55,7 +55,7 @@ public:
 	}
 
 	virtual void SetEncCfg(const std::string& str) {
-		std::ofstream cfg(std::string("encoder_console.cfg"));
+		std::ofstream cfg(std::string("encoder.cfg"));
 		cfg << str;
 	}
 	virtual void SetMDCfg(const std::string& str) {
@@ -68,19 +68,153 @@ public:
 	}
 };
 
+void dumpRegs()
+{
+	int fd;
+
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+		log() << "cannot open /dev/mem. su?";
+		return;
+    }
+
+    void * volatile map_base = mmap(0, 1024, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0x04000000);
+
+	uint8_t* regs = (uint8_t*)map_base;
+	
+	for (int i=0; i<0x30; i+=2) {
+		log() << std::hex << "reg 0x" << i << " : 0x" << *(uint16_t*)(regs+i);
+	}
+	
+	log() << std::dec;
+	
+    munmap((void*)map_base, 1024);
+    close(fd);
+}
+
+void restartFpga()
+{
+	int fd;
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+		log() << "cannot open /dev/mem. su?";
+		return;
+    }
+
+/*FPGA RESET:
+# cd /sys/class/gpio
+# echo 49 > export
+# cd /sys/class/gpio/gpio49
+# echo "out" > direction
+# echo 1 > value
+# echo 0 > value*/
+
+	void * volatile map_base = mmap(0, 1024, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0x04000000);
+
+	const int regs_num = 0x30/2;
+	short regs[regs_num];
+
+	uint16_t* p = (uint16_t*)map_base;
+	for(int i = 0; i<regs_num; i++)
+		regs[i] = *p++;
+
+	FILE* fexport = fopen("/sys/class/gpio/export", "wt");
+	fprintf(fexport, "49");
+	fclose(fexport);
+	
+	FILE* fdir = fopen("/sys/class/gpio/gpio49/direction", "wt");
+	fprintf(fdir, "out");
+	fclose(fdir);
+
+	FILE* fval = fopen("/sys/class/gpio/gpio49/value", "wb");
+	fprintf(fval, "1\n");
+	fflush(fval);
+	boost::this_thread::sleep_for(boost::chrono::milliseconds(50));
+	fprintf(fval, "0\n");
+	fclose(fval);
+
+	p = (uint16_t*)map_base;
+	log() << "Firmware version: " << std::hex << *(p+0x2e/2) << std::dec;
+	for(int i = 0; i<regs_num; i++)
+		*p++ = regs[i];
+
+    munmap((void*)map_base, 1024);
+    close(fd);
+}
+
 void initMD()
 {
+	restartFpga();
+	
 	std::ifstream cfg(std::string("md.cfg"));
 
 	int val1 = -1, val2 = -1, val3 = -1, val4 = -1, val5 = -1;
 
-	cfg >> val1;
-	cfg >> val2;
-	cfg >> val3;
-	cfg >> val4;
-	cfg >> val5;
+	cfg >> val1 >> val2 >> val3 >> val4 >> val5;
 
 	log() << "MD config : " << val1 << ", " << val2 << ", " << val3 << ", " << val4 << ", " << val5; 
+
+	if (val1 < 0 || val2 < 0 || val3 < 0 || val4 < 0 || val5 < 0) {
+		dumpRegs();
+		return;
+	}
+
+    int fd;
+
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+		log() << "cannot open /dev/mem. su?";
+		return;
+    }
+
+    void * volatile map_base = mmap(0, 1024, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0x04000000);
+
+	uint8_t* regs = (uint8_t*)map_base;
+	
+	const double base = 10;
+	
+	const double time_thres = (pow(base, val1/100.0) - 1.)/(base - 1.) * (0.5 - 0.035) + 0.035;
+	const int time_thres_i = time_thres * (1<<18) / 64;
+	*(uint16_t*)(regs + 0x014) = time_thres_i & 0xffff;
+	*(uint16_t*)(regs + 0x016) = time_thres_i>>16;
+	
+	const int spat_thres = val2 * (13 - 1) / 100 + 1;
+	const int tmp = *(uint16_t*)(regs + 0x01A);
+	*(uint16_t*)(regs + 0x01A) = tmp & ~0x00f0 | (spat_thres << 4 & 0x01f0);
+
+	const double top_t = (pow(base, val3/100.0) - 1.)/(base - 1.) * (1.-1./256.) + 1./256.;
+	const int top_t_i = top_t * (1<<18);
+	*(uint16_t*)(regs + 0x000) = top_t_i & 0xffff;
+	*(uint16_t*)(regs + 0x002) = top_t_i>>16;
+
+	const double bot_t = (pow(base, val4/100.0) - 1.)/(base - 1.) * (1.-0) + 0;
+	const int bot_t_i = bot_t * (1<<18);
+	*(uint16_t*)(regs + 0x004) = bot_t_i & 0xffff;
+	*(uint16_t*)(regs + 0x006) = bot_t_i>>16;
+
+	const double noise = (pow(base, val5/100.0) - 1.)/(base - 1.) * (0.08-0.005) + 0.005;
+	const double noise2 = noise*noise;
+	const double inoise = 1./noise/16.;
+
+	const int noise_i = noise * (1<<18);
+	const int noise2_i = noise2 * (1<<18);
+	const int inoise_i = inoise * (1<<18);
+
+	*(uint16_t*)(regs + 0x00c) = noise_i & 0xffff;
+	*(uint16_t*)(regs + 0x00e) = noise_i>>16;
+	*(uint16_t*)(regs + 0x008) = noise2_i & 0xffff;
+	*(uint16_t*)(regs + 0x00a) = noise2_i>>16;
+	*(uint16_t*)(regs + 0x010) = inoise_i & 0xffff;
+	*(uint16_t*)(regs + 0x012) = inoise_i>>16;
+
+
+	log() << "Time thres: " << time_thres << " (0x" << std::hex << time_thres_i << std::dec << 
+			"), spat thres: " << spat_thres << " (0x" << std::hex << spat_thres << std::dec << 
+			"), top temp: " << top_t << " (0x" << std::hex << top_t_i << std::dec << 
+			"), bot temp: " << bot_t << " (0x" << std::hex << bot_t_i << std::dec << 
+			"), noise: " << noise << " (0x" << std::hex << noise_i << std::dec << ")";
+
+    munmap((void*)map_base, 1024);
+    close(fd);
+	
+	dumpRegs();
 }
 
 void loadMask(std::vector<uint8_t>& info_mask, int s, int h)
@@ -190,10 +324,12 @@ void fillInfo(std::vector<uint8_t>& info, const std::vector<uint8_t>& info_mask,
 		uint8_t *p = data + y*s;
 		for (int x=0; x<w; x+=16, p+=16) {
 			uint8_t b = 0;
-			for (int i=0; i<16; i++)
+			for (int i=0; i<16; i++) {
 				if (p[i] > 0x80)
-					b |= 1 << i/2;
-					
+					b |= 1 << 7-i/2;
+				p[i] = 0x80;
+			}
+
 			*it_info++ = b;
 		}
 	}
@@ -244,15 +380,15 @@ void run()
 
 		size_t coded_size=0;
 
+		fillInfo(info, info_mask, (uint8_t*)(buf.m.userptr + w*h), w, w, h/2); // data is in chroma planes.
+
 		XDAS_Int8* bs = enc.encFrame((XDAS_Int8*)buf.m.userptr, w, h, w, &coded_size);
 
 		cap.putFrame(buf);
 
-		fillInfo(info, info_mask, (uint8_t*)(buf.m.userptr + w*h), w/2, w/2, h); // data is in chroma planes.
+//fwrite((uint8_t*)&info[0], 1, w/2*h/2/8, f_dump_info);
 
-//fwrite((uint8_t*)&info[0], 1, w/2*h/8, f_dump_info);
-
-		const uint8_t* cur = encode_frame(&info[0], &info_out[0], w, h);
+		const uint8_t* cur = encode_frame(&info[0], &info_out[0], w/2, h/2);
 
 		const ptrdiff_t info_size = cur - &info_out[0];
 
@@ -308,6 +444,8 @@ int main(int argc, char *argv[])
 	catch (ex& e) {
 		log() << e.str();
 	}
+	
+	::system("sync");
 
 	return 0;
 }
